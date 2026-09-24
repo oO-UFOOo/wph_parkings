3957923ff24f743d330dbe72385c33fe44d38498
dkokas <doki__21@msn.com>
2026-09-24 12:52:49 +0300

    add sopa homepage

--- src/main/java/com/sopa/controller/SecurityConfig.java
+++ src/main/java/com/sopa/controller/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.sopa.controller;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
     @Bean
